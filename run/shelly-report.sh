75913439aeaf500532012159e2794d25b4ebabc6
Lauri Junkkari <latenssi@gmail.com>
2023-07-12 16:45:50 +0300

    Lint shelly-report script

--- run/shelly-report.sh
+++ run/shelly-report.sh
@@ -2,12 +2,12 @@
 
 # Shelly Report
 
-R_ID=$(echo $REPORT | jq -r '.ID')
-R_BATTERY=$(echo $REPORT | jq -r '.bat')
-R_LUX=$(echo $REPORT | jq -r '.lux')
-R_MOTION=$(echo $REPORT | jq -r '.motion')
-R_TAMPER=$(echo $REPORT | jq -r '.tamper')
-R_TEMP=$(echo $REPORT | jq -r '.temp')
+R_ID=$(echo "$REPORT" | jq -r '.ID')
+R_BATTERY=$(echo "$REPORT" | jq -r '.bat')
+R_LUX=$(echo "$REPORT" | jq -r '.lux')
+R_MOTION=$(echo "$REPORT" | jq -r '.motion')
+R_TAMPER=$(echo "$REPORT" | jq -r '.tamper')
+R_TEMP=$(echo "$REPORT" | jq -r '.temp')
 
 DATA="shellyReport,sensor_id=${R_ID} battery=${R_BATTERY},lux=${R_LUX},motion=${R_MOTION},tamper=${R_TAMPER},temperature=${R_TEMP} $(date +%s)"
 
